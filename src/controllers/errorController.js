577275e6fda184494c5776f3246ede48d0c18c88
Hisham Elmorsi <hmelmorsi@gmail.com>
2023-10-01 06:52:46 +0300

    Implementation(v1) of Error handling system

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -14,48 +14,68 @@ const handleDuplicateFields = (err)=> {
     return new AppError(message, 400)
 }
 
+const handleValidationError = (err) => {
+    const errors = Object.values(err.errors)
+    const errorsMessages = errors.map(err => err.message)
+
+    const message = `Invalid Input Data. ${errorsMessages.join('. ')}`
+    return new AppError(message, 400)
+}
+
+const handleJWTError = () => new AppError('Invalid Token, please login again', 401)
+const handleExpiredToken = () => new AppError('Expired Token, please login again', 401)
+
+
+const sendDevError = (err, req, res) => {
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+
+}
+
+const sendProdError = (err, req, res) => {
+    if(err.isOperational) {
+
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+
+    } else {
+
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: "OOPS, Something went wrong!"
+        })
+
+    }
+}
+
 module.exports = (err, req, res, next) => {
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
     if(process.env.NODE_ENV === 'development') {
-
-        res.status(err.statusCode).json({
-            status: err.status,
-            error: err,
-            message: err.message,
-            stack: err.stack
-        })
-
         console.error('Error 💥', err)
+        sendDevError(err, req, res);
     }
 
     if(process.env.NODE_ENV === 'production') {
+        console.error('Error 💥', err)
+
         let error = { ...err }
-        console.error('Error 💥', err)
-
         if(err.name === 'CastError') error = handleCastError(error);
+        if(err.name === 'ValidationError') error = handleValidationError(error)
         if(err.code === 11000) error = handleDuplicateFields(error)
+        if(err.name === 'JsonWebTokenError') error = handleJWTError()
+        if(err.name === 'TokenExpiredError') error = handleExpiredToken()
 
-        
-        if(error.isOperational) {
-
-            res.status(error.statusCode).json({
-                status: error.status,
-                message: error.message
-            })
-
-        } else {
-
-            res.status(error.statusCode).json({
-                status: error.status,
-                message: "Something went wrong!"
-            })
-
-        }
-
-        console.error('Error 💥', err)
+        sendProdError(error, req, res)
 
     }
 
